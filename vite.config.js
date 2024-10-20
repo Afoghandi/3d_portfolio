9827438c0996087c60d288a2c9441f2ee03b23a7
afoghandi <ralphaeloshun@gmail.com>
2024-10-20 21:45:19 +0100

    updated deploymeny config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/3d_portfolio/',
-  plugins: [react()],
+  plugins: [react(), visualizer(), viteImagemin({
+    gifsicle: { optimizationLevel: 7 },
+    optipng: { optimizationLevel: 7 },
+    webp: { quality: 75 },
+  }),],
 })
